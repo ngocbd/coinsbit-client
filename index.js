96bf4d6e1da101c6bb969389ab6867e16a7f830b
NgocBD <buidinhngoc.aiti@gmail.com>
2023-02-04 10:09:19 +0700

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+class CoinsbitClient{
+    constructor ()
+    {
+      this.request = axios.create({
+        baseURL: 'https://coinsbit.io',
+        timeout: 10000,
+        headers: {'Content-Type': 'application/json'}
+      });
+    }
+    async getMarkets() {
+      const response = await this.request.get('/api/v1/public/markets');
+      return response.data;
+    }
+    version() {
+      return "1.0.0";
+    }
+}
+
+exports = module.exports = () =>{ return new CoinsbitClient()};
+exports.version = () =>{ return "1.0.0"};
---
+++ test/test.js
@@ -0,0 +1,16 @@
+const chai = require('chai')
+const expect = chai.expect
+const coinsbitClient = require('../index.js')()
+
+describe("CoinsbitClient Tests", () => {
+
+	//test a function for a specific case
+	it("should return client version", ()=> {
+		expect(coinsbitClient.version()).equals("1.0.0");
+	})
+	it("should return markets", async ()=> {
+		const markets = await coinsbitClient.getMarkets();
+		console.log(markets);
+		expect(markets).to.be.an('array');
+	})
+})
