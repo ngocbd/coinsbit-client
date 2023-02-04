ac3b676aec850a446c92328178b96dfcc1f6a88c
namdh2607 <halala1102@gmail.com>
2023-02-04 10:27:52 +0700

    update test

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const axios = require('axios');
-class CoinsbitClient{
-    constructor ()
-    {
-      this.request = axios.create({
-        baseURL: 'https://coinsbit.io',
-        timeout: 10000,
-        headers: {'Content-Type': 'application/json'}
-      });
-    }
-    async getMarkets() {
-      const response = await this.request.get('/api/v1/public/markets');
-      return response.data;
-    }
-    version() {
-      return "1.0.0";
-    }
+class CoinsbitClient {
+  constructor() {
+    this.request = axios.create({
+      baseURL: 'https://coinsbit.io',
+      timeout: 10000,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
+  async getMarkets() {
+    const response = await this.request.get('/api/v1/public/markets');
+    return response.data;
+  }
+  async getTicker() {
+    const response = await this.request.get('/api/v1/public/tickers');
+    return response.data;
+  }
+
+  version() {
+    return "1.0.0";
+  }
 }
 
-exports = module.exports = () =>{ return new CoinsbitClient()};
-exports.version = () =>{ return "1.0.0"};
+exports = module.exports = () => { return new CoinsbitClient() };
+exports.version = () => { return "1.0.0" };
--- test/test.js
+++ test/test.js
@@ -5,12 +5,17 @@ const coinsbitClient = require('../index.js')()
 describe("CoinsbitClient Tests", () => {
 
 	//test a function for a specific case
-	it("should return client version", ()=> {
+	it("should return client version", () => {
 		expect(coinsbitClient.version()).equals("1.0.0");
 	})
-	it("should return markets", async ()=> {
+	it("should return markets", async () => {
 		const markets = await coinsbitClient.getMarkets();
 		console.log(markets);
-		expect(markets).to.be.an('array');
+		expect(markets).to.be.an('object');
+	})
+	it('should return ticker', async () => {
+		const ticker = await coinsbitClient.getTicker();
+		
+		expect(ticker).to.be.an('object');
 	})
 })
