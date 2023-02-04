c1183c82a434e6c6bb1743d43df31e4a84d81307
namdh2607 <halala1102@gmail.com>
2023-02-04 11:37:10 +0700

    update test

--- index.js
+++ index.js
@@ -7,14 +7,68 @@ class CoinsbitClient {
       headers: { 'Content-Type': 'application/json' }
     });
   }
+
+  // Public API
   async getMarkets() {
     const response = await this.request.get('/api/v1/public/markets');
     return response.data;
   }
-  async getTicker() {
+  async getTickers() {
     const response = await this.request.get('/api/v1/public/tickers');
     return response.data;
   }
+
+  async getTicker() {
+    const response = await this.request.get('/api/v1/public/ticker');
+    return response.data;
+  }
+
+  async getBook() {
+    const response = await this.request.get('/api/v1/public/book');
+    return response.data;
+  }
+  async getHistory() {
+    const response = await this.request.get('/api/v1/public/history');
+    return response.data;
+  }
+  async getHistoryResult() {
+    const response = await this.request.get('/api/v1/public/history/result');
+    return response.data;
+  }
+
+  async getProducts() {
+    const response = await this.request.get('/api/v1/public/products');
+    return response.data;
+  }
+
+  async getSymbols() {
+    const response = await this.request.get('/api/v1/public/symbols');
+    return response.data;
+  }
+  async getDepthResult() {
+    const response = await this.request.get('/api/v1/public/depth/result');
+    return response.data;
+  }
+  async getKline() {
+    const response = await this.request.get('/api/v1/public/kline');
+    return response.data;
+  }
+
+  // Market API
+  // async getNew() {
+  //   const response = await this.request.get('/api/v1/order/new');
+  //   return response.data;
+  // }
+
+  // async getCancel() {
+  //   const response = await this.request.get('/api/v1/order/cancel');
+  //   return response.data;
+  // }
+
+  // async getOrder() { 
+  //   const response = await this.request.get('/api/v1/orders');
+  //   return response.data;
+  // }
 
   version() {
     return "1.0.0";
--- test/test.js
+++ test/test.js
@@ -5,17 +5,73 @@ const coinsbitClient = require('../index.js')()
 describe("CoinsbitClient Tests", () => {
 
 	//test a function for a specific case
+
+
+	// Public API
+
 	it("should return client version", () => {
 		expect(coinsbitClient.version()).equals("1.0.0");
 	})
 	it("should return markets", async () => {
 		const markets = await coinsbitClient.getMarkets();
-		console.log(markets);
+		// console.log(markets);
 		expect(markets).to.be.an('object');
+	})
+	it('should return tickers', async () => {
+		const tickers = await coinsbitClient.getTickers();
+		expect(tickers).to.be.an('object');
 	})
 	it('should return ticker', async () => {
 		const ticker = await coinsbitClient.getTicker();
-		
 		expect(ticker).to.be.an('object');
 	})
+	it('should return books', async () => {
+		const book = await coinsbitClient.getBook();
+		expect(book).to.be.an('object');
+	})
+	it('should return history', async () => {
+		const history = await coinsbitClient.getHistory();
+		expect(history).to.be.an('object');
+	});
+	it('should return historyResult', async () => {
+		const historyResult = await coinsbitClient.getHistoryResult();
+		expect(historyResult).to.be.an('object');
+	});
+	it('should return products', async () => {
+		const products = await coinsbitClient.getProducts();
+		expect(products).to.be.an('object');
+	})
+	it('should return symbols', async () => {
+		const symbols = await coinsbitClient.getSymbols();
+		expect(symbols).to.be.an('object');
+	});
+	it('should return depthResults', async () => {
+		const depthResults = await coinsbitClient.getDepthResult();
+		expect(depthResults).to.be.an('object');
+	});
+	it('should return kline', async () => {
+		const kline = await coinsbitClient.getKline();
+		expect(kline).to.be.an('object');
+	});
+
+	// Market API
+
+	// it('should return new', async () => {
+	// 	const orderNew = await coinsbitClient.getNew();
+	// 	expect(orderNew).to.be.an('object');
+	// });
+
+	// it('should return cancel', async () => {
+	// 	const orderCancel = await coinsbitClient.getCancel();
+	// 	expect(orderCancel).to.be.an('object');
+	// });
+
+	// it('should return orders', async () => {
+	// 	const orders = await coinsbitClient.getOrders();
+	// 	console.log(orders);
+	// 	expect(orders).to.be.an('object');
+	// });
+	
+
+	// AccountAPI
 })
